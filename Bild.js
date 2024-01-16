a9cd9c57ee0d9235a295570b8896a6829fb3fcab
Polytetrafluoroethylene <ninjati17@gmail.com>
2024-01-16 12:28:48 +0100

    hot fix

---
+++ Bild.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("Drueck-mich");
+button.addEventListener("click", hallo);
+const invisible = document.getElementsByClassName(invisible);
+function hallo(){
+  const bild = document.getElementById("bild")
+  invisible.classList.remove("invisible");
+ }
--- Spin.js
+++
@@ -1,3 +0,0 @@
-const span = document.getElementById("span")
-span.addEventListener ("click",spon)
-function  spon(){spin.classList.add("spin")}
