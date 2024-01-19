7de22e3bddefb7efa296aa99d3b33f8c0f032850
Polytetrafluoroethylene <ninjati17@gmail.com>
2024-01-19 14:43:47 +0100

    Verknüpfung

--- Bild.js
+++ Bild.js
@@ -1,7 +1,1 @@
-const button = document.getElementById("Drueck-mich");
-button.addEventListener("click", hallo);
-const invisible = document.getElementsByClassName(invisible);
-function hallo(){
-  const bild = document.getElementById("bild")
-  invisible.classList.remove("invisible");
- }
+onclick=document.getElementById
